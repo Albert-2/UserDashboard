e0bb250733392ac8c5c837b2e51594e4bccaffb0
Bhushan <alberteinstein02.99792458>
2024-12-27 10:15:50 +0530

    Enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import multer from 'multer';
+// import multer from 'multer';
 import { ValidationPipe } from '@nestjs/common';
-import cors from 'cors';
+// import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,15 +17,13 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // // Configure Multer middleware for file handling
-  app.use(
-    multer({
-      storage: multer.memoryStorage(), // Store files in memory
-      limits: { fileSize: 10 * 1024 * 1024 }, // Set file size limit (10 MB)
-    }).single('file'),
-  );
+  // app.use(
+  //   multer({
+  //     storage: multer.memoryStorage(),
+  //     limits: { fileSize: 10 * 1024 * 1024 },
+  //   }).single('file'),
+  // );
 
-  // Start listening on port 5000
   await app.listen(5000);
 }
 
